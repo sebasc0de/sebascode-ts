bc4648b615e9ab43a733e38dbe531ff3ee825cea
sebasc0de <nahuelsebastiandiaz@icloud.com>
2022-03-17 12:06:00 -0300

    Language toggler state init changes

--- src/components/LanguageToggler/LanguageToggler.tsx
+++ src/components/LanguageToggler/LanguageToggler.tsx
@@ -11,22 +11,22 @@ export const LanguageToggler = () => {
   };
 
   return (
-    <div className="fixed bottom-2 left-3 z-10 bg-white rounded-md drop-shadow-lg py-1 px-3 cursor-pointer">
+    <div className="fixed bottom-2 left-3 z-20 bg-white rounded-md drop-shadow-lg py-1 px-3 cursor-pointer">
       {inEnlgish ? (
-        <button
-          onClick={() => toggleLanguageHandler('en')}
-          className="flex gap-2 items-center"
-        >
-          <img src="/img/argentina.png" className="w-5" alt="" />
-          <p className="text-xs text-gray-500 font-medium">Español</p>
-        </button>
-      ) : (
         <button
           onClick={() => toggleLanguageHandler('es')}
           className="flex gap-2 items-center"
         >
           <img src="/img/eeuu.png" className="w-5" alt="" />
           <p className="text-xs text-gray-500 font-medium">English</p>
+        </button>
+      ) : (
+        <button
+          onClick={() => toggleLanguageHandler('en')}
+          className="flex gap-2 items-center"
+        >
+          <img src="/img/argentina.png" className="w-5" alt="" />
+          <p className="text-xs text-gray-500 font-medium">Español</p>
         </button>
       )}
     </div>
