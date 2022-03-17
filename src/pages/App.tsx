ff1434a39117b6d0c83f8c4fac339c26a1922310
sebasc0de <nahuelsebastiandiaz@icloud.com>
2022-03-17 11:17:44 -0300

    Testimonial carrousel to hidden

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,6 @@
 import '../../src/App.css';
 import { HeroSection } from '../components/HeroSection/HeroSection';
+import { LanguageToggler } from '../components/LanguageToggler/LanguageToggler';
 import { Meet } from '../components/Meet/Meet';
 import { Portfolio } from '../components/Portfolio/Portfolio';
 import { Skills } from '../components/Skills/Skills';
@@ -8,11 +9,12 @@ import { TestimonialCarrousel } from '../components/Testimonials/TestimonialsCar
 function App() {
   return (
     <>
+      <LanguageToggler />
       <HeroSection />
       <Skills />
       <Portfolio />
       <Meet />
-      <TestimonialCarrousel />
+      {/* <TestimonialCarrousel /> */}
     </>
   );
 }
