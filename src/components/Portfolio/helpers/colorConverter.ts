d5c04b7ea9403bf9dd25224ed6d06d8f9e5846ec
sebasc0de <nahuelsebastiandiaz@icloud.com>
2022-03-17 11:16:48 -0300

    Moved to portfolio helpers folder

---
+++ src/components/Portfolio/helpers/colorConverter.ts
@@ -0,0 +1,36 @@
+export const colorConverter = (techonology: string): string => {
+  switch (techonology) {
+    case 'wp':
+      return 'bg-blue-500';
+
+    case 'html':
+      return 'bg-red-500';
+
+    case 'php':
+      return 'bg-cyan-500';
+
+    case 'bootstrap':
+      return 'bg-purple-500';
+
+    case 'elementor':
+      return 'bg-pink-500';
+
+    case 'woocommerce':
+      return 'bg-violet-500';
+
+    case 'reactjs':
+      return 'bg-cyan-500';
+
+    case 'firebase':
+      return 'bg-orange-500';
+
+    case 'redux':
+      return 'bg-violet-500';
+
+    case 'commercejs':
+      return 'bg-green-500';
+
+    default:
+      return 'bg-gray-500';
+  }
+};
