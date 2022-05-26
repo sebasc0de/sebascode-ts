10460fe4f834a6f5538343c957333669702ec7ac
sebasc0de <nahuelsebastiandiaz@icloud.com>
2022-05-26 13:48:14 -0300

    Fix routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { I18nextProvider } from 'react-i18next';
-import { ReactJS } from './pages/ReactJS';
-import App from './pages/App';
-import global_en from './translations/en/global.json';
-import global_es from './translations/es/global.json';
-import i18next from 'i18next';
-import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { I18nextProvider } from "react-i18next";
+import App from "./pages/App";
+import global_en from "./translations/en/global.json";
+import global_es from "./translations/es/global.json";
+import i18next from "i18next";
+import ReactDOM from "react-dom";
 
 i18next.init({
   interpolation: { escapeValue: false },
-  lng: 'es',
+  lng: "es",
   resources: {
     es: {
       global: global_es,
@@ -25,9 +24,8 @@ ReactDOM.render(
     <I18nextProvider i18n={i18next}>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/react" element={<ReactJS />} />
       </Routes>
     </I18nextProvider>
   </BrowserRouter>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
