ef5753b216a523b840eade3a5497c576493b1c87
sebasc0de <nahuelsebastiandiaz@icloud.com>
2022-06-01 21:28:30 -0300

    Loaders images routes

---
+++ src/data/Loaders.ts
@@ -0,0 +1,10 @@
+export const Loaders = [
+  "./loaders/1.png",
+  "./loaders/2.png",
+  "./loaders/3.png",
+  "./loaders/4.png",
+  "./loaders/5.png",
+  "./loaders/6.png",
+  "./loaders/7.png",
+  "./loaders/8.png",
+];
